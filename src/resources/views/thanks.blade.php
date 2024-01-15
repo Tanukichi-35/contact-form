b71b5c9ae79e494de932b200838c1ef47781acc5
Tanukichi-35 <yo-fu@outlook.jp>
2024-01-15 10:02:19 +0900

    add layout

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,14 +1,29 @@
+@extends('layouts.app')
+
+@section('css')
+  <link rel="stylesheet" href="{{ asset('css/thanks.css') }}" />
+@endsection
+
+@section('content')
+<div class="thanks__content">
+  <div class="thanks__heading">
+    <h2>お問い合わせありがとうございます</h2>
+  </div>
+</div>
+@endsection
+
+<style>/*
 <!DOCTYPE html>
 <html lang="ja">
 
-<head>
+<!-- <head>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Contact Form</title>
   <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
   <link rel="stylesheet" href="{{ asset('css/thanks.css') }}" />
-</head>
+</head> -->
 
 <body>
   <header class="header">
@@ -29,3 +44,4 @@
 </body>
 
 </html>
+*/</style>
